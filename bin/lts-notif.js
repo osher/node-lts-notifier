49ff00e959a445b4c37c8b4027792c4b3e67aae2
Osher El-Netanany <osher.filter@gmail.com>
2018-12-03 18:41:53 +0200

    leave debug out

--- bin/lts-notif.js
+++ bin/lts-notif.js
@@ -1,6 +1,6 @@
-process.env.DEBUG = 'lts-notif*';
+require('colors');
 Promise.resolve()
 .then(() => require('../lib/config')({}))
 .then((cfg) => require('../lib')(cfg))
 .then((r) => console.log('done', r))
-.catch((e) => console.log('oups...\n', e));
+.catch((e) => console.log('oups...\n'.red, e));
